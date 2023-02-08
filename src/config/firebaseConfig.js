9618340a2299e3d90bafff517d3a5d5d5ee483c8
vonatlus <52702565+adil-sultanov@users.noreply.github.com>
2023-02-08 20:47:10 +0900

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,20 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCXctbugXwhPVKIZOaceQzaAOJJRBwlEIk",
-  authDomain: "koma-crypto.firebaseapp.com",
-  projectId: "koma-crypto",
-  storageBucket: "koma-crypto.appspot.com",
-  messagingSenderId: "15638166710",
-  appId: "1:15638166710:web:bd5ebf920321d2424c3a17"
+  apiKey: "AIzaSyBJ4sT7lBk34wnqGjiWj5-Y-f3BR6meOFQ",
+  authDomain: "fir-test-bruh.firebaseapp.com",
+  databaseURL: "https://fir-test-bruh-default-rtdb.firebaseio.com",
+  projectId: "fir-test-bruh",
+  storageBucket: "fir-test-bruh.appspot.com",
+  messagingSenderId: "442662439642",
+  appId: "1:442662439642:web:3f2c88859c2125fbb8f3e3"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCXctbugXwhPVKIZOaceQzaAOJJRBwlEIk",
+//   authDomain: "koma-crypto.firebaseapp.com",
+//   projectId: "koma-crypto",
+//   storageBucket: "koma-crypto.appspot.com",
+//   messagingSenderId: "15638166710",
+//   appId: "1:15638166710:web:bd5ebf920321d2424c3a17"
+// };
 
 export default firebaseConfig;
