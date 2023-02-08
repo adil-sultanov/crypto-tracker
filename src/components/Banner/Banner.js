6ec411a063bee8fc5a143ef7dbc18f7c64f36cba
vonatlus <52702565+adil-sultanov@users.noreply.github.com>
2023-02-08 21:47:09 +0900

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Koma Crypto
+            vonatlus Crypto Tracker
           </Typography>
           <Typography
             variant="subtitle2"
