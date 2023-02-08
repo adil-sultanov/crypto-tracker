90d7cdabcc3e20b5b27f797ff31cd53571d2766d
vonatlus <52702565+adil-sultanov@users.noreply.github.com>
2023-02-09 03:08:37 +0900

    Update AuthModal.js

--- src/components/Authentication/AuthModal.js
+++ src/components/Authentication/AuthModal.js
@@ -2,13 +2,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
-import { Button, Tab, Tabs, AppBar, Box } from "@material-ui/core";
+import { Button, Tab, Tabs, AppBar} from "@material-ui/core";
 import Signup from "./Signup";
 import Login from "./Login";
 import { useState } from "react";
 import { CryptoState } from "../../CryptoContext";
 import { auth } from "../../firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -22,23 +22,11 @@ const useStyles = makeStyles((theme) => ({
     color: "white",
     borderRadius: 10,
   },
-  google: {
-    padding: 24,
-    paddingTop: 0,
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-    marginLeft: '19%',
-    gap: 20,
-    fontSize: 20,
-  },
 }));
 
 export default function AuthModal() {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-
-  const { setAlert } = CryptoState();
 
   const handleOpen = () => {
     setOpen(true);
