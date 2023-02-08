31003967e8ed10863d57bcddeb246ef47e2ba4b9
vonatlus <52702565+adil-sultanov@users.noreply.github.com>
2023-02-09 03:18:14 +0900

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -8,13 +8,4 @@ const firebaseConfig = {
   appId: "1:442662439642:web:3f2c88859c2125fbb8f3e3"
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCXctbugXwhPVKIZOaceQzaAOJJRBwlEIk",
-//   authDomain: "koma-crypto.firebaseapp.com",
-//   projectId: "koma-crypto",
-//   storageBucket: "koma-crypto.appspot.com",
-//   messagingSenderId: "15638166710",
-//   appId: "1:15638166710:web:bd5ebf920321d2424c3a17"
-// };
-
 export default firebaseConfig;
