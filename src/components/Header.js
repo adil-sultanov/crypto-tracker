51694fc53b1b6b5095ab38dd1500580b8a71af9d
vonatlus <52702565+adil-sultanov@users.noreply.github.com>
2023-02-09 03:16:34 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,7 +62,7 @@ function Header() {
                 onChange={(e) => setCurrency(e.target.value)}
               >
                 <MenuItem value={"USD"}>USD - $</MenuItem>
-                <MenuItem value={"KZT"}>KZT - ₸</MenuItem>
+                <MenuItem value={"KZT"}>KZT - ₸</MenuItem> 
                 <MenuItem value={"KRW"}>KRW - ₩</MenuItem>
               </Select>
 
